f4402b32cca16e64671ff1c10648aa218873aa31
ys2843 <ys2843@nyu.edu>
2018-06-13 23:16:53 -0400

    update

--- background.js
+++ background.js
@@ -1,9 +1,27 @@
 chrome.commands.onCommand.addListener(function (command) {
     console.log('Command:', command);
     chrome.tabs.query({url: "https://www.youtube.com/*"}, function (tabs) {
-        console.log(tabs[0].id);
-        chrome.tabs.sendMessage(tabs[0].id, {method: "next"}, function (response) {
+        console.log(tabs[0]);
+        chrome.tabs.sendMessage(tabs[0].id, {method: command}, function (response) {
+            console.log(response);
+            if (response) {
+                console.log("next success")
+                chrome.tabs.query({url: "https://www.youtube.com/*"}, function (tabs) {
+                    console.log("tab success: " + tabs[0].title);
+                    if (tabs[0]) {
+                        chrome.notifications.create('Next song', {
+                                type: 'basic',
+                                iconUrl: './images/get_started128.png',
+                                title: 'Now playing',
+                                message: "heo"
+                            },
+                            function (id) {
+                                console.log(chrome.runtime.lastError)
+                            }
+                        );
+                    }
+                });
+            }
         });
     });
 });
-
--- content.js
+++ content.js
@@ -1,5 +1,16 @@
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
-    if (message.method === 'next') {
-        window.location.href = document.querySelector('a.ytp-next-button').href;
+    // console.log('message.method: ' + message.method)
+    switch(message.method) {
+        case 'next':
+            window.location.href = document.querySelector('a.ytp-next-button').href;
+            sendResponse({message: 'nextFinish'})
+            break;
+        case 'stop':
+            document.querySelector('button.ytp-play-button').click();
+            break;
+        case 'like':
+            document.querySelectorAll("a.yt-simple-endpoint.style-scope.ytd-toggle-button-renderer")[0].click();
+            break;
+
     }
 });
